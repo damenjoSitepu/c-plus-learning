c0c375871fcc5f6b7632adbd528afd13c91f43d9
damenjoSitepu <damenstory@gmail.com>
2021-09-11 19:32:58 +0700

    Membuat File Data Type

--- C++/HelloWorld/HelloWorld.cpp
+++ C++/HelloWorld/HelloWorld.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "My Name Is Damenjo Sitepu"<<endl;
+    cout << "Today Is Saturday"<<endl;
+    cout << "Embedded Application";
     system("pause>0");
 }
+
---
+++ Data Type/Data Type.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float mySallary;
+    cout << "Input Your Sallary : "; cin >> mySallary;
+
+    cout << "My Salary Was : " << mySallary << endl;
+
+    system("pause>0");
+}
+
