21b6a7eb05fd4b142449060971ebca0eeec4bc45
damenjoSitepu <damenstory@gmail.com>
2021-09-14 20:38:34 +0700

    Membuat Latihan Program ASCII Untuk Beginner

---
+++ ASCII Program Beginner/ASCII Program Beginner.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char v1, v2, v3, v4, v5;
+
+    //Input
+    cout << "ASCII Program For Beginner 1" << endl;
+    cin >> v1 >> v2 >> v3 >> v4 >> v5;
+    //Output
+    cout << "---------------------------------" << endl;
+    cout << "The Result Is : " << int(v1) << " " << int(v2) << " " << int(v3) << " " << int(v4) << " " << int(v5) << endl;
+
+    system("pause>0");
+}
+
